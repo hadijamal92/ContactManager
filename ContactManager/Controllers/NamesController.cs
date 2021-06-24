3c1128979537dbbb486d7407e7e2cc1f77b72a57
Hadi Jamal <hadijamal000@gmail.com>
2021-06-23 22:18:24 -0400

    adding mvc

---
+++ ContactManager/Controllers/NamesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Data;
+using ContactManager.Models;
+
+namespace ContactManager.Controllers
+{
+    public class NamesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Names
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Name.ToListAsync());
+        }
+
+        // GET: Names/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var name = await _context.Name
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (name == null)
+            {
+                return NotFound();
+            }
+
+            return View(name);
+        }
+
+        // GET: Names/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Names/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FullName,PhoneNumber")] Name name)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(name);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(name);
+        }
+
+        // GET: Names/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var name = await _context.Name.FindAsync(id);
+            if (name == null)
+            {
+                return NotFound();
+            }
+            return View(name);
+        }
+
+        // POST: Names/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,PhoneNumber")] Name name)
+        {
+            if (id != name.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(name);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NameExists(name.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(name);
+        }
+
+        // GET: Names/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var name = await _context.Name
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (name == null)
+            {
+                return NotFound();
+            }
+
+            return View(name);
+        }
+
+        // POST: Names/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var name = await _context.Name.FindAsync(id);
+            _context.Name.Remove(name);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NameExists(int id)
+        {
+            return _context.Name.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ContactManager/Models/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class Name
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Name()
+        {
+
+        }
+    }
+}
